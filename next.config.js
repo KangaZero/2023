90b3ff96fa83fa3b1e9c41b891aa7be0a9a513d3
Samuel <samuelyongw@gmail.com>
2023-01-28 23:06:32 +1100

    yml added

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 
 const withPWA = require('next-pwa');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = withPWA({
   pwa: {
     dest: 'public',
@@ -9,6 +11,13 @@ module.exports = withPWA({
     skipWaiting: true,
   },
 });
+
+module.exports = {
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
 const nextConfig = {
   experimental: {
